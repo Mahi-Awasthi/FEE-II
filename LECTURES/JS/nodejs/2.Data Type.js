8b2dc4dc42be74a1f3c4cea4594881281d2ea832
MahiAwasthi <mahi2057.be23@chitkara.edu.in>
2024-08-23 15:23:27 +0530

    .

---
+++ LECTURES/JS/nodejs/2.Data Type.js
@@ -0,0 +1,16 @@
+let a=10; //number
+let aa=-5; //number
+let aaa=2.5; //number
+let b="hello"; //string
+let c=false; //boolean
+d=20;//value of d
+let j=null;//object
+
+
+console.log(typeof(a))
+console.log(typeof(aa))
+console.log(typeof(aaa))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(j))
