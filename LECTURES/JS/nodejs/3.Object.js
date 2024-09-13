a533378c2577a6193b7137ff85cc3a488fe09cca
MahiAwasthi <mahi2057.be23@chitkara.edu.in>
2024-09-13 14:58:22 +0530

    .

---
+++ LECTURES/JS/nodejs/3.Object.js
@@ -0,0 +1,16 @@
+let a=[1,2,3,4,5,6,7]
+// console.log(a.splice(1,3)) output: [ 2, 3, 4 ]
+console.log(a.splice(2,3,6,6))
+console.log(a)
+
+// There are 2 ways to declare an object: constructor/literals:
+//1. constructors
+const obj=new Object();
+obj.name="MAHI"
+console.log(typeof obj)
+console.log(obj)
+
+// 2. literals
+user={name:"Mahi",rollno:22,subject:"CPP"}
+console.log(user.name)
+
