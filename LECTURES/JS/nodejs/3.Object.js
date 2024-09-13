c8565fe8a3acfce8f33ae8b03860020c21b60296
MahiAwasthi <mahi2057.be23@chitkara.edu.in>
2024-09-13 15:22:29 +0530

    .

--- LECTURES/JS/nodejs/3.Object.js
+++ LECTURES/JS/nodejs/3.Object.js
@@ -1,7 +1,7 @@
-let a=[1,2,3,4,5,6,7]
+// let a=[1,2,3,4,5,6,7]
 // console.log(a.splice(1,3)) output: [ 2, 3, 4 ]
-console.log(a.splice(2,3,6,6))
-console.log(a)
+// console.log(a.splice(2,3,6,6)) output: [3, 4, 5]
+// console.log(a) output: [1, 2, 6, 6, 6, 7]
 
 // There are 2 ways to declare an object: constructor/literals:
 //1. constructors
@@ -11,6 +11,32 @@ console.log(typeof obj)
 console.log(obj)
 
 // 2. literals
-user={name:"Mahi",rollno:22,subject:"CPP"}
-console.log(user.name)
+const user={
+    name:"Mahi",
+    'last name':"Awasthi", //if there's a space in between key. we can use '' symbol
+    rollno:22,
+    subject:"CPP"
+}
 
+// Change object value:
+user.name="Mehak"
+
+// Accessing object value:
+console.log(user['name'])
+console.log(user['last name'])
+
+// Object inside an object:
+let arr=[1,2,[1,2,3,4],34]
+console.log(arr[2],[2])
+
+let user1={
+    user2: {
+        name:'Mahi',
+        subject:'CPP',
+    },
+    user3: {
+        name:"Awasthi",
+        subject:"OS",
+    }
+};
+console.log(user1.user3.name)
